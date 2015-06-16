73f05ddc1f23541f841e269d2d5dbd2251e59f93
Greg Burghardt <greg_burghardt@yahoo.com>
2015-06-16 13:38:59 -0400

    Demo of MS Test bug. Solution built with Visual Studio 2010. Bug does not exist.

---
+++ MsTestBugDemo/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MsTestBugDemo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+#line 1 "UnitTest1.foo"
+            Assert.IsTrue(true);
+        }
+    }
+}
